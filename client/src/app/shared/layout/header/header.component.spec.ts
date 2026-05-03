fcb26fc0f0c421d36622153f2594d965c670e29d
Bogomaz Dmitry <bogomaz.dima2013@gmail.com>
2026-05-03 21:55:56 +0500

    fix: add missing providers and NO_ERRORS_SCHEMA to component specs
    - header.component.spec: mock AuthService/UserService, add RouterTestingModule
    - signup-form.component.spec: mock AuthService/SignUpFormService, add ReactiveFormsModule

--- client/src/app/shared/layout/header/header.component.spec.ts
+++ client/src/app/shared/layout/header/header.component.spec.ts
@@ -1,5 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
 import { HeaderComponent } from './header.component';
+import { AuthService } from '../../services/auth.service';
+import { UserService } from '../../services/user.service';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -7,7 +11,13 @@ describe('HeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [ HeaderComponent ],
+      imports: [ RouterTestingModule ],
+      providers: [
+        { provide: AuthService, useValue: { isAuthorize: () => false } },
+        { provide: UserService, useValue: {} },
+      ],
+      schemas: [ NO_ERRORS_SCHEMA ],
     })
     .compileComponents();
   });
--- client/src/app/user/signup-form/signup-form.component.spec.ts
+++ client/src/app/user/signup-form/signup-form.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
 import { SignUpFormComponent } from './signup-form.component';
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { SignUpFormService } from './signup-form.service';
 
 describe('RegistrFormComponent', () => {
   let component: SignUpFormComponent;
@@ -9,6 +12,21 @@ describe('RegistrFormComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [SignUpFormComponent],
+      imports: [ ReactiveFormsModule ],
+      providers: [
+        { provide: AuthService, useValue: { signUp: () => {} } },
+        { provide: SignUpFormService, useValue: {
+          formGroup: new FormGroup({
+            userName: new FormControl(''),
+            email: new FormControl(''),
+            password: new FormControl(''),
+          }),
+          globalError: '',
+          getFormField: () => new FormControl(''),
+          handleErrors: () => {},
+        } },
+      ],
+      schemas: [ NO_ERRORS_SCHEMA ],
     }).compileComponents();
   });
 
